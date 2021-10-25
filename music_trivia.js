090111a1faab52519a903995aca9b2ba58706be5
Dan Smestad <hockeydan25@gmail.com>
2021-10-24 22:35:16 -0500

    Midterm work, html,js,css pages working need to reorder my radio buttons remove input box.

---
+++ music_trivia.js
@@ -0,0 +1,35 @@
+let triviaQuestion = document.querySelector('#music-question')
+let userAnswer = document.querySelector('#user-answer')
+let submitButton = document.querySelector('#submit-button')
+let nextQuestion = document.querySelector('#next-question-button')
+let trueChoice = document.querySelector('#true-choice')
+let falseChoice = document.querySelector('#false-choice')
+let correctAnswer = document.querySelector('#answer')
+
+let questionAndAnswerApi = `https://opentdb.com/api.php?amount=5&category=12&difficulty=easy&type=boolean`
+
+// function getNewQuestion => {
+//     let triviaQuestion = queston.length
+
+
+// }
+
+
+// document.addEventListener('keyup', function() {
+//     if (event.keyCode == 13 || event.key == 'Enter'){    //if statment for eventlistener to take action on if true.
+//         submitButton.click() //this is the action the enter button takes, cuases a button click function.
+//     }
+// })
+/* loading the first question fetching the url and getting to the first set of question
+and answers object array[0].question.if i refresh the page it loads a new set*/ 
+fetch(questionAndAnswerApi)
+    .then( (res) => {
+    return res.json()
+    }).then( (questionAndAnswerJson) => {
+
+    let questionArray = questionAndAnswerJson.results[0].question
+        //console.log(questionArray)
+        triviaQuestion.innerHTML = questionArray
+
+
+    })    
